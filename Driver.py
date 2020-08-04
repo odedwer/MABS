ed31fde9054b18a82c5a918f5e53db67d3043630
odedwer <odedwer@gmail.com>
2020-08-04 22:44:37 +0300

    done more tests. Ready for integration

--- Driver.py
+++ Driver.py
@@ -6,18 +6,19 @@ from Simulation import Simulation
 import matplotlib.pyplot as plt
 
 # %%
-N = 10
-K = 2
-T = 10000
-POSSIBLE_REWARDS = np.array([1, 2, 3])
+N = 100
+K = 10
+T = 1000
+POSSIBLE_REWARDS = np.arange(10)
 
 get_reward_probabilities = lambda: np.random.dirichlet(np.flip(1 + np.arange(len(POSSIBLE_REWARDS))))
-
+# %%
 fig = plt.figure(figsize=(24, 6))
-row1, row2 = fig.subplots(2, 2)
+row1, row2, row3 = fig.subplots(3, 2)
 entropy_ax, ent_gain_ax = row1
 normal_ax, baseline_ax = row2
-SEED = 98  # same seeds
+ts_norm_ax, ts_ent_ax = row3
+SEED = 10  # same seeds
 np.random.seed(SEED)
 simEntropy = Simulation(N, K, T, POSSIBLE_REWARDS, get_reward_probabilities, ModelType.UCB_ENTROPY)
 resEntropy = simEntropy.run_simulation()
@@ -37,15 +38,25 @@ np.random.seed(SEED)
 simBaseline = Simulation(N, K, T, POSSIBLE_REWARDS, get_reward_probabilities, ModelType.BASELINE_MODEL)
 resBaseline = simBaseline.run_simulation()
 simBaseline.plot_choice_distributions(baseline_ax)
+
+np.random.seed(SEED)
+simTSEnt = Simulation(N, K, T, POSSIBLE_REWARDS, get_reward_probabilities, ModelType.THOMPSON_ENTROPY)
+resTSEnt = simTSEnt.run_simulation()
+simTSEnt.plot_choice_distributions(ts_ent_ax)
+
+np.random.seed(SEED)
+simTSNorm = Simulation(N, K, T, POSSIBLE_REWARDS, get_reward_probabilities, ModelType.THOMPSON_NORMAL)
+resTSNorm = simTSNorm.run_simulation()
+simTSNorm.plot_choice_distributions(ts_norm_ax)
 # %%
 model_name = "UCB"
 fig.savefig("%s choices comparison.png" % model_name)
 
 # %%
 window_size = 300
-sim_list = [simNormal, simEntropy, simEntGain, simBaseline]
+sim_list = [simNormal, simEntropy, simEntGain, simBaseline, simTSEnt, simTSNorm]
 # %% plot convergence
-fig = vis.plot_convergences(sim_list,window_size)
+fig = vis.plot_convergences(sim_list)
 # fig.savefig("%s convergence rate.png" % model_name)
 # %% plot cumulative reward
 fig = vis.plot_rewards(sim_list)
